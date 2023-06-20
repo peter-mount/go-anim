25884fbd9c0be377dc4b532818d4a4b9fe5ffeb3
peter <peter@retep.org>
2023-06-20 11:04:35 +0100

    testCardAnim demo

---
+++ demo/testCardAnim.c
@@ -0,0 +1,33 @@
+
+include "testcard/uhd.c"
+include "testcard/uhd-countdown-slider.c"
+
+main() {
+    println( "Demo Test Card UHD with slider" )
+
+    frameRate   := 30 // Frames per second
+    duration    := 10 // Duration of animation
+
+    ctx:= animGraphic.NewContext()
+
+    totalFrames := frameRate * duration
+
+    try( encoder := ffmpeg.New( "test.mp4", frameRate ) ) {
+        for frame:=0; frame< totalFrames; frame=frame+1 {
+            // The shared state
+            state := testCardUHD_Init(ctx)
+            state.frameRate = frameRate
+            state.frameNumber = math.Float(frame)
+
+            testCardUHD_Base(ctx, state)
+            testCardUHD_Lower(ctx, state)
+            testCardUHD_Slicer_Lower(ctx, state)
+            testCardUHD_Upper(ctx, state)
+            testCardUHD_Slicer_Upper(ctx, state)
+            testCardUHD_Top(ctx, state)
+
+            encoder.WriteImage(ctx.Image())
+        }
+    }
+
+}
---
+++ include/testcard/uhd-countdown-slider.c
@@ -0,0 +1,48 @@
+# uhd-countdown-slider.c
+#
+# This displays a simple slider which expands for each frame with one cycle
+# representing a single second.
+#
+# With uhd.c this is in two parts, the lower one is rendered after testCardUHD_Lower and
+# the second after testCardUHD_Upper.
+#
+# It also requires the following entries in state:
+#
+# frameRate     number of frames per second.
+# frameNumber   the frame number being rendered.
+#
+
+testCardUHD_Slicer_Lower(ctx, s) {
+    try( ctx ) {
+        gc := ctx.Gc()
+
+        animGraphic.FillRectangle(gc,
+            s.dW+(5.8*s.dx), s.dH+(0.6*s.dy),
+            3.4*s.dx, s.dy*0.8,
+            s.white)
+    }
+}
+
+testCardUHD_Slicer_Upper(ctx, s) {
+    try( ctx ) {
+        gc := ctx.Gc()
+
+        // convert into seconds [0] and frame in second [1]
+        frameRate:= math.Float(s.frameRate)
+        second := math.Modf( math.Float(s.frameNumber) / frameRate )
+		sz := second[1]
+
+        animGraphic.FillRectangle(gc,
+			s.dW+(6.4*s.dx), s.dH+(0.8*s.dy),
+			2.2*s.dx, s.dy*0.4,
+			s.black)
+
+		if sz > 0.0001 {
+			//sz = sz + (1 / frameRate)
+			animGraphic.FillRectangle(gc,
+				s.dW+(6.4*s.dx)+10, s.dH+(0.8*s.dy+10),
+				(2.2*s.dx-20)*sz, s.dy*0.4-20,
+				s.lightgreen)
+		}
+    }
+}
